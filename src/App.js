8ce294caaa30bc78aef64f0896662ee12b7927e5
Neha <nehashaikh470@gmail.com>
2022-07-01 00:29:22 +0200

    data fetch and design implement

--- src/App.js
+++ src/App.js
@@ -1,71 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-function Navbar() {
-  // return <div className="navbar">
-  return <div className="Header">
-  <div className="inner_header">
-      <div className="logo_container" >
-          <a href=""><img src="./img/S3_Logo_aw.png" alt="SoundThree" style={"width: 200px; height:60px"}/></a>
-          {/* <!-- <h1>Sound<span>3</span></h1> --> */}
-      </div>
-      <ul className="navigation">
-          <a href=""><li>Home</li></a>
-          <a href=""><li>Browse</li></a>
-          <a href=""><li>Genres</li></a>
-          <a href=""><li>Whatever</li></a>
-          <a href=""><li>Something</li></a>
-          <a href=""><li>Else</li></a>
+import Songs from "./Songs";
+import Navbar from "./Navbar";
 
-          <form action="" className="search_box">
-              <input type="text" placeholder="Search" id="" />
-              <button type="submit">🔎︎</button>
-          </form>
-      </ul>
- </div>
-</div>
-}
-
-function Sidebar() {
-  const [cats, setCats] = useState(temp_cats);
-  return (
-    <div className="sidebar">
-      {cats.map((c) => (
-        <div className="cat">{c.en}</div>
-      ))}
-    </div>
-  );
-}
-
-function SongList() {
-  const [songs, setSongs] = useState(temp_songs);
-  useEffect(() => {
-    // fetch("http://localhost:8000/songs")
-    //   .then((res) => res.json())
-    //   .then((data) => setSongs(data));
-  }, []);
-  return (
-    <div className="song-list">
-      {temp_songs.map((song) => (
-        <div className="song">
-          <div>▶</div>
-          <div>❤️</div>
-          <div>{song.name}</div>
-          <div>{song.artist}</div>
-          <div>{song.genre}</div>
-          <div>{song.BPM}</div>
-          <div>{song.duration}</div>
-        </div>
-      ))}
-    </div>
-  );
-}
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="main">
-        <Sidebar />
-        <SongList />
+        <Songs />
       </div>
     </div>
   );
---
+++ src/Songs.js
@@ -0,0 +1,45 @@
+import react, {useState, useEffect} from "react";
+
+function Songs() {
+    const [song, setSong] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setLoading(true)
+        const fetchMusic = () =>{
+        fetch("https://soundthree-backend.herokuapp.com/data")
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data)
+            setSong(data)})
+            .catch((error) => console.log(error))
+        }
+        
+        console.log(fetchMusic())
+      }, []);
+
+      useEffect(()=>{
+        setLoading(false)
+    },[song])
+
+    return(
+            <div className="song-list">
+                  <div>{song && song.map((sg) => {
+                    return(
+                        <div className="song">
+                            <div><img src={sg.image} /></div>
+                            <div>{sg.title}</div> 
+                            <div>{sg.singer}</div>
+                            <div>▶</div>
+                            <div>❤️</div>
+                        </div>
+                    )
+                })}
+             </div>       
+            
+            </div>
+        
+    
+    )
+}
+
+export default Songs;
